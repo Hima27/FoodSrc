247a1169e2e89fdeca1aed645827a9febbf63402
Hima Patel <hima@uoguelph.ca>
2021-02-13 23:28:31 -0500

    combined different elements

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
     res.sendFile('index.html',{ root: __dirname });
+    
 });
 app.listen(1080, function () {
   console.log('go to localhost:1080 to see site');
